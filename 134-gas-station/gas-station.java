b654152229f8d1c506ae5acd95b25ff8c335f846
Roohee Singh <113611360+Roohee13@users.noreply.github.com>
2026-02-22 12:09:43 +0530

    Time: 3 ms (32.83%) | Memory: 119.6 MB (24.20%) - LeetSync

---
+++ 134-gas-station/gas-station.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalTank=0,currentTank=0,startIndex=0;
+        
+        for(int i=0;i<gas.length;i++){
+            int diff= gas[i]-cost[i];
+            totalTank = totalTank + diff;
+            currentTank = currentTank + diff;
+            
+            if(currentTank<0){
+                startIndex=i+1;
+                currentTank=0;
+            }
+        }
+
+        return totalTank>=0 ? startIndex : -1;
+    }
+}
